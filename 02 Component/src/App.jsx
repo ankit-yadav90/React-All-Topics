94e39b1dc45b9adc1b5b054944d1bc9361a1fa02
ankit-yadav90 <ankityadav73699@gmail.com>
2025-11-26 20:11:30 +0530

    react component

---
+++ 02 Component/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Nave from "./component/nav";
+
+const App = () => {
+  return (
+    <>
+      <div>App</div>;<h1>hellow gyes</h1>
+      <Nave />
+      <Nave />
+      <Nave />
+      <Nave />
+    </>
+  );
+};
+
+export default App;
---
+++ 02 Component/src/component/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Nave = () => {
+  return (
+    <div className="nav">
+      <h1>Hellow Dada</h1>
+      <p>
+        hellow i am chaching to the component Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Assumenda, tempore.
+      </p>
+      <h5>test</h5>
+    </div>
+  );
+};
+export default Nave;
